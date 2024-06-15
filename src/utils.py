264fafab74a5449019eb7c169b6bed82ea4f250a
realowner <iamrealowner@gmail.com>
2024-06-15 12:38:50 +0300

    readme added"

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 
-
 def item_exists(list_: list, index: int) -> str:
     if index > len(list_):
         return 'Вышли за пределы списка'
     return f'Ваш элемент: {list_[index]}'
 
+
+if __name__ == '__main__':
+    print(item_exists([1, 2], 1))
